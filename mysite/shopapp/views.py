3b17a499af70ef00a94bd1e0e2eae3e456a75c4a
Никита Выскубов <vysnik@gmail.com>
2023-07-24 15:55:21 +0300

    Выполнил условия 2.6 Практическая работа

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,16 @@
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('laptop', 1999),
+        ('desktop', 2999),
+        ('smartphone', 999),
+    ]
+    context = {
+        'time_running': default_timer(),
+        'products': products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
